8a8c29a7a586617a37a77e89a8eebe4078b40459
andrewpbray <andrewbray@berkeley.edu>
2023-01-06 16:50:12 -0800

    simplify directory

--- _extensions/andrewpbray/scheduled-posts/scheduled-posts.lua
+++
@@ -1,9 +0,0 @@
--- Toggle draft to true if the date of the doc is in the future
-function Meta(m)
-  if m.date ~= nil then
-    if tonumber(m.date[1].text) > os.time() then 
-      m["draft"] = true
-      return m
-    end
-  end
-end
