aafc1ec6856c36721abbea7d41d6c36b78af192d
andrewpbray <andrewbray@berkeley.edu>
2023-01-09 17:20:54 -0800

    clean up

--- edit_render_list.R
+++ edit_render_list.R
@@ -1,7 +1,10 @@
 # Reader in full render list
-render_list <- Sys.getenv("QUARTO_PROJECT_INPUT_FILES")
-render_list <- "posts/topic-3/notes.qmd\nposts/topic-2/notes.qmd\nposts/topic-1/notes.qmd\nindex.qmd\nposts.qmd"
-render_list <- unlist(strsplit(render_list, "\n"))
+full_render_list <- Sys.getenv("QUARTO_PROJECT_INPUT_FILES")
+
+# Print for debugging
+full_render_list
+
+full_render_list <- unlist(strsplit(full_render_list, "\n"))
 
 # Read in schedule data
 x <- yaml::read_yaml("_schedule.yml")
@@ -16,8 +19,9 @@ is_future_doc <- function(x) {
 future_docs <- unlist(lapply(x, is_future_doc))
 
 # Remove future docs from render list and set variable
-partial_render_list <- setdiff(render_list, future_docs)
-Sys.setenv("QUARTO_PROJECT_INPUT_FILES" = paste(partial_render_list, collapse = "\n"))
+partial_render_list <- paste(setdiff(full_render_list, future_docs), collapse = "\n")
+Sys.setenv("QUARTO_PROJECT_INPUT_FILES" = partial_render_list)
+# write(paste(partial_render_list, collapse = "\n"), "render_list.file")
 
 # Print to help debug
-Sys.getenv("QUARTO_PROJECT_INPUT_FILES")
+partial_render_list
